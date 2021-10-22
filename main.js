43fef31f3d6bbae88a86ed6fa413f87b2ed7b6e0
Victor Oliveira <92695998+VictorOliveira2k@users.noreply.github.com>
2021-10-21 21:37:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'VictorOliveira2k',
+  youtube: 'maykbrito',
+  facebook: 'profile.php?id=100005098389130',
+  instagram: 'fn_v.oliveira',
+  twitter: 'Victorsouza2k'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
